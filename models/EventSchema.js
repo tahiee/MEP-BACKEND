069cc1fab767af1715bc243c8e011c472e0f6452
tahiee <niazi.tahirkhan15@gmail.com>
2024-08-19 15:30:12 +0500

    a

--- models/EventSchema.js
+++ models/EventSchema.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose');
+  const mongoose = require('mongoose');
 
-const eventSchema = new mongoose.Schema({
-  eventname: { type: String, required: true },
-  eventdate: { type: Date, },
-  description: String,
-  banner: {
-    data: Buffer,
-    contentType: String
-  },
-  audience: String,
-  type: { type: String, },
-  price: { type: Number, },
-  tech: { type: String, },
-  agenda: String,
-  hostname: String,
-  email: { type: String, },
-  country: String,
-  address: String,
-  city: String,
-  socialLinks: {
-    website: String,
-    instagram: String
-  },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-});
+  const eventSchema = new mongoose.Schema({
+    eventname: { type: String, required: true },
+    eventdate: { type: Date, },
+    description: String,
+    banner: {
+      data: Buffer,
+      contentType: String
+    },
+    audience: String,
+    type: { type: String, },
+    price: { type: Number, },
+    tech: { type: String, },
+    agenda: String,
+    hostname: String,
+    email: { type: String, },
+    country: String,
+    address: String,
+    city: String,
+    socialLinks: {
+      website: String,
+      instagram: String
+    },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  });
 
-module.exports = mongoose.model('eventdetails', eventSchema);
+  module.exports = mongoose.model('eventdetails', eventSchema);
